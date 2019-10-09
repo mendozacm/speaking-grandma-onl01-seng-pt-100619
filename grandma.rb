68569831589ac617beb813298a96a16b2f8af7b1
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-09 14:21:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+     "NO, NOT SINCE 1938!"
     elsif phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
   else
